0f425045bd86cbedf78d4ce8f8c0be728f103fbe
PraveenkumarB46 <151357767+PraveenkumarB46@users.noreply.github.com>
2025-06-22 21:05:11 +0530

    Create ques6.java

---
+++ 22-06-2025/ques6.java
@@ -0,0 +1,5 @@
+public class LinearSearch {
+    public static void main(String[] args) {
+        int[] arr = {5, 8, 2, 9, 1};
+        int key = 9, index = -1;
+        for (int i = 0; i < arr.length; i++) {
